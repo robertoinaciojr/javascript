6855822cf9759cbaf346e630c8d60f4af94f6e70
Roberto Inácio dos Santos Junior <robertoinacio789@gmail.com>
2022-03-10 16:13:01 -0300

    Criando os ex008,ex009,ex010

---
+++ aula006/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80
+
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if (vel > 60){ //Condição simples!
+    console.log(`Você ultrapassou da velocidade permitida. MULTADO!`)
+}
+
+console.log(`Diriga com segurança`)
